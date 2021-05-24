dcb64c554970b03be22b7749c8b145722058ea45
Sandra Binoy <sandra.binoy@outlook.com>
2021-05-24 19:54:20 +0530

    Initial commit (Project setup)

---
+++ src/test/java/com/sortingvisualizer/algorithms/SortingVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sortingvisualizer.algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortingVisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
